b54598fdac8778ba27a1d072f05361e324e1b870
Pablosancval <pablosancval@gmail.com>
2022-01-14 09:05:43 +0100

    Ex 1 2 3 4

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,13 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +15,17 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class FeedingType {
-    Integer id;
+@Table(name = "feeding_types", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
+public class FeedingType extends BaseEntity{
+
+    @Size(min = 5, max = 30)
+	@Column(name = "name")
     String name;
+
+    @NotNull
     String description;
+
+    @NotNull
+    @ManyToOne
     PetType petType;
 }
