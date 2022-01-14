b7a4b47df2d8f032dd3fb1c89cf6c7f588c4c352
Pablosancval <pablosancval@gmail.com>
2022-01-14 09:08:10 +0100

    included control test files for ease of access

---
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.product;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product extends NamedEntity{
+
+    @PositiveOrZero
+    @NotNull
+    double price;
+
+    @ManyToOne
+    ProductType productType;
+}
---
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.product;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public List<ProductType> getAllProductTypes(){
+        return productRepository.findAllProductTypes();
+    }
+
+    public List<Product> getProductsCheaperThan(Double price) {
+        return productRepository.findByPriceLessThan(price);
+    }
+
+    public ProductType getProductType(String typeName) {
+        return productRepository.findProductTypeByName(typeName);
+    }
+
+    public Product save(Product p){
+        return null;       
+    }
+
+    
+}
